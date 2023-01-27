28e0745bd340b89a480b0baeb2d12a7d5459d80a
Astr0clad <colin.montigel@gmail.com>
2023-01-27 14:55:59 -0700

    Add rrTesting class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/rrTestingAuton.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.util.PIDController;
+
+@Autonomous(name="RR Testing")
+public class rrTestingAuton extends LinearOpMode {
+
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90)); // Set start pose to center of the field, facing north
+
+    public static double Kp = 0.01, Ki = 0, Kd = 0;
+    public static int targetInches = 0;
+
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+    DcMotorEx liftEncoder;
+    CRServo liftMotor;
+
+    PIDController control = new PIDController(Kp, Ki, Kd, dashboardTelemetry);
+
+    @Override
+    public void runOpMode() {
+        liftEncoder = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        liftMotor = hardwareMap.crservo.get("vertical"); // Ensure Spark Mini is on Braking
+
+        liftEncoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                .forward(24)
+                .turn(Math.toRadians(90))
+                .addDisplacementMarker(() -> {
+                    targetInches = 37;
+                })
+                .strafeRight(24)
+                .build();
+
+        waitForStart();
+
+        if (!isStopRequested()) {
+            drive.followTrajectorySequence(trajSeq);
+        }
+
+        while(opModeIsActive()) {
+            int targetPosition = (int)(targetInches * 64.68056888);
+            // Update pid controller
+            double command = control.update(targetPosition, liftEncoder.getCurrentPosition());
+            command = Range.clip(command, -1, 1);
+            // Assign PID output
+            dashboardTelemetry.addData("Command", command);
+            liftMotor.setPower(command);
+
+            dashboardTelemetry.update();
+        }
+    }
+}
